76622c14a9d007d643a6a9205c6d2183782bddb0
bartugkeskn <bartukeskin1999@gmail.com>
2024-11-04 22:26:15 +0300

    Movie ve Context sınıfları oluşturuldu. Migration işlemleri yapıldı.Code first yaklaşımı ile Category tablosu eklendi. - bartuğ

---
+++ Project4_EntityFrameworkCodeFirstMovie/DAL/Context/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Project4_EntityFrameworkCodeFirstMovie.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4_EntityFrameworkCodeFirstMovie.DAL.Context
+{
+    public class MovieContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; } // Movie isimli sınıfımı sql'de Movies isimli tabloya dönüştür.
+        public DbSet<Category> Categories { get; set; }
+    }
+}
