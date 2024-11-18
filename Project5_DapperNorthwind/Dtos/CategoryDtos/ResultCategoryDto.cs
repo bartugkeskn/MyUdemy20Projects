24e68c145d81a9e965c2f0ab7d518aa78e038206
bartugkeskn <bartukeskin1999@gmail.com>
2024-11-18 15:51:10 +0300

    ...

---
+++ Project5_DapperNorthwind/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5_DapperNorthwind.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto   // Listeleme işlemi için kullanılacak dto'muz bu
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
